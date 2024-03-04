edcf26faf5688914dccb5d90ee50ac6126debc61
hamed005 <hamednobakht949@gmail.com>
2024-03-05 00:20:39 +0330

    update-1.2

--- src/main/java/org/example/persianexpress/HelloController.java
+++ src/main/java/org/example/persianexpress/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
 
 
     public void onloginBtnClicked(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Customers/CustomersPanel.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Pages/Customers/CustomersPanel.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setTitle("Persian Express");
