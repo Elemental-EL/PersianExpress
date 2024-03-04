f78f2ba39d6e77e1c82664631a4eec95c40de7d8
Elemental-EL <elyar.k@yahoo.com>
2024-03-04 22:35:45 +0330

    Update-1.1/Adding pages...

---
+++ src/main/java/org/example/persianexpress/CreateAccountController.java
@@ -0,0 +1,37 @@
+package org.example.persianexpress;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class CreateAccountController {
+    @FXML
+    private Button submitBtn;
+    @FXML
+    private ChoiceBox<String> typeSlct;
+    @FXML
+    private TextField firstNameText, familyNameText, nCodeText, fatherNameText, userText, bPlaceText, codePText, mPhoneText, hPhoneText;
+    @FXML
+    private DatePicker bDate;
+    @FXML
+    private TextArea addressText;
+    @FXML
+    private PasswordField passText, passRepText;
+    @FXML
+    private ImageView logoBtn, backBtn;
+
+    @FXML
+    void onLogoClicked(MouseEvent event){
+
+    }
+    @FXML
+    void onBackClicked(MouseEvent event){
+
+    }
+    @FXML
+    void onSubmitClicked(ActionEvent event){
+
+    }
+}
