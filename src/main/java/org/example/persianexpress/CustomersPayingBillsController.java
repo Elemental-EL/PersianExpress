402711e31bb43c9df6c5e536de77dbe5ddc7d3fe
helianasimi <helianasimi418@gmail.com>
2024-03-19 22:09:09 +0330

    Revert "update-2.72"
    This reverts commit 2c2f4eed04749a25922f5b75ab973dc94d1b71f2.

--- src/main/java/org/example/persianexpress/CustomersPayingBillsController.java
+++ src/main/java/org/example/persianexpress/CustomersPayingBillsController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -18,8 +17,6 @@ public class CustomersPayingBillsController {
     private Scene scene;
     @FXML
     private TextField billEarmark , paymentEarmark , billAmount;
-    @FXML
-    private ChoiceBox<String> SelectedAccount;
     public void onBackclicked(MouseEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Pages/Customers/CustomersPanel.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
