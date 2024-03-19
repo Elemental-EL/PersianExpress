8be6d05a7fb72836a4d7fbec8e7724aa3fcd6a71
helianasimi <helianasimi418@gmail.com>
2024-03-19 23:08:52 +0330

    paying biils-erors

--- src/main/java/org/example/persianexpress/CustomersPayingBillsController.java
+++ src/main/java/org/example/persianexpress/CustomersPayingBillsController.java
@@ -35,5 +35,17 @@ public class CustomersPayingBillsController {
     }
 
     public void onPaymentClicked(ActionEvent event) {
+        if (billEarmark.getText().isEmpty()||paymentEarmark.getText().isEmpty()||billAmount.getText().isEmpty()){
+            errorText.setText("پر کردن تمامی فیلد ها الزامی است.");
+        } else if (billAmount.getText().length()!=8||paymentEarmark.getText().length()!=8) {
+            errorText.setText("شناسه قبض و شناسه پرداخت باید 8 رقمی باشند.");
+        }
+        else if (!billEarmark.getText().matches("\\d+")||!paymentEarmark.getText().matches("\\d+")||!billAmount.getText().matches("\\d+")){
+            errorText.setText("شناسه قبض و شناسه پرداخت و مبلغ قبض فقط باید شامل عدد باشد.");
+        } else if (!billEarmark.getText().matches("1403\\d{4}")) {
+errorText.setText("فرمت شناسه قبض اشتباه است.");
+        } else if (!paymentEarmark.getText().matches("1234\\d{4}")) {
+   errorText.setText("فرمت شناسه پرداخت اشتباه است");
+        }
     }
 }
